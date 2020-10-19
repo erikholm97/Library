fdb986ad2be60c3f0dbf64864cc4aaed30653981
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-10-19 22:35:08 +0200

    First commit

---
+++ LibraryBackEnd/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsCEO { get; set; }
+
+        public bool IsManager { get; set; }
+
+        public int ManagerId { get; set; }
+    }
+}
---
+++ LibraryBackEnd/LibraryItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Library
+{
+    public class LibraryItem
+    {
+        public int Id { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Pages { get; set; }
+
+        public int RunTimeMinutes { get; set; }
+
+        public bool IsBorrowable { get; set; }
+
+        public string Borrower { get; set; }
+
+        public DateTime BorrowDate { get; set; }
+
+        public int Type { get; set; }
+
+
+    }
+}
