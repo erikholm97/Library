ba2deac6002d12ac47e402ff29eb278689f3fa4f
Erik Holm <erik.holm@netigrate.se>
2020-10-20 12:41:36 +0200

    ASP.net with EntityCore connection

---
+++ LibraryFrontEnd/Controllers/LibraryItemsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Library;
+using LibraryBackEnd;
+
+namespace LibraryFrontEnd.Controllers
+{
+    public class LibraryItemsController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public LibraryItemsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LibraryItems
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LibraryItem.ToListAsync());
+        }
+
+        // GET: LibraryItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libraryItem = await _context.LibraryItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libraryItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(libraryItem);
+        }
+
+        // GET: LibraryItems/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LibraryItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,Pages,RunTimeMinutes,IsBorrowable,Borrower,BorrowDate,Type")] LibraryItem libraryItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libraryItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libraryItem);
+        }
+
+        // GET: LibraryItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libraryItem = await _context.LibraryItem.FindAsync(id);
+            if (libraryItem == null)
+            {
+                return NotFound();
+            }
+            return View(libraryItem);
+        }
+
+        // POST: LibraryItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Author,Pages,RunTimeMinutes,IsBorrowable,Borrower,BorrowDate,Type")] LibraryItem libraryItem)
+        {
+            if (id != libraryItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libraryItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibraryItemExists(libraryItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libraryItem);
+        }
+
+        // GET: LibraryItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libraryItem = await _context.LibraryItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libraryItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(libraryItem);
+        }
+
+        // POST: LibraryItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libraryItem = await _context.LibraryItem.FindAsync(id);
+            _context.LibraryItem.Remove(libraryItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibraryItemExists(int id)
+        {
+            return _context.LibraryItem.Any(e => e.Id == id);
+        }
+    }
+}
