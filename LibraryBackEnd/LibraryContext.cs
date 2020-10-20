ffdbbac24315d2c95569b8809220e9651b76d99b
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-10-20 07:21:48 +0200

    Entity Core Framework

---
+++ LibraryBackEnd/LibraryContext.cs
@@ -0,0 +1,26 @@
+﻿using Library;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryBackEnd
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Employees> Employees { get; set; }
+
+        public DbSet<LibraryItem> LibraryItem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Integrated Security=True");
+        }
+    }
+}
+
+
+
---
+++ LibraryBackEnd/Migrations/20201020052032_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryBackEnd.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(nullable: false),
+                    IsCEO = table.Column<bool>(nullable: false),
+                    IsManager = table.Column<bool>(nullable: false),
+                    ManagerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibraryItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Pages = table.Column<int>(nullable: false),
+                    RunTimeMinutes = table.Column<int>(nullable: false),
+                    IsBorrowable = table.Column<bool>(nullable: false),
+                    Borrower = table.Column<string>(nullable: true),
+                    BorrowDate = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibraryItem_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryItem_CategoryId",
+                table: "LibraryItem",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "LibraryItem");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
