91a9c1034115649117005f721bac58d01c87695f
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-10-20 21:50:45 +0200

    Enum for library item

---
+++ LibraryBackEnd/Migrations/20201020194857_createfinal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryBackEnd.Migrations
+{
+    public partial class createfinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
